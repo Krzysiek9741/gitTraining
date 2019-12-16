38ae7c5ce8bbf18e5b6256295d9750b434ff8784
Falkowski Krzysztof <k.falkowski@op.pl>
2019-12-16 21:50:03 +0100

    next commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(add(1, 3));
+    }
+
+    static int add(int a, int b){
+        return a+b;
+    }
 }
